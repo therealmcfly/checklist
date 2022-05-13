25d213ef900fa88a23070dd1b1e9b8888ab9c098
therealmcfly <eugene.fly.lee@gmail.com>
2022-05-13 22:20:22 +0900

    ver_01.03

--- src/ListOfItems.js
+++ src/ListOfItems.js
@@ -1,14 +1,22 @@
-function Item({ index, content }) {
+import Button from "./Button";
+
+function Item({ id, index, content, onDelete }) {
+  const handleDeleteItem = () => onDelete(id);
   return (
-    <li>
-      {index + 1}. {content}
-    </li>
+    <>
+      <li key={id}>
+        {index + 1}. {content}
+      </li>
+      <Button onClick={handleDeleteItem}>x</Button>
+    </>
   );
 }
 
-function ListOfItems({ list }) {
+function ListOfItems({ list, onDelete }) {
   return list.map((item, i) => (
-    <Item key={item.key} index={i} content={item.content} />
+    <>
+      <Item id={item.id} index={i} content={item.content} onDelete={onDelete} />
+    </>
   ));
 }
 export default ListOfItems;
